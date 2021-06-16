5baaea079cc8914059e025749b22a276ab991aa0
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2021-06-16 12:38:51 -0400

    some errors after completion

--- src/io/github/nathannorth/worstHello/Main.java
+++ src/io/github/nathannorth/worstHello/Main.java
@@ -2,6 +2,14 @@ package io.github.nathannorth.worstHello;
 
 public class Main {
     public static void main(String[] args) {
+        //todo find the first thousand prime numbers and sort them by their sum of digits
+        //todo a big loading bar
         System.out.println("Hello World!");
+        throw new TaskFailedSuccessfullyException();
+    }
+    private static class TaskFailedSuccessfullyException extends RuntimeException {
+        public TaskFailedSuccessfullyException() {
+            throw new TaskFailedSuccessfullyException();
+        }
     }
 }
